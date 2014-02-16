6509fe91dcf1e7cd0bc3e5600d56488b76dc93f5
YAMASAKI Masahide <masahide.y@gmail.com>
2014-02-16 16:44:41 +0900

    gofmt -w

--- pyramid-scheme_test.go
+++ pyramid-scheme_test.go
@@ -7,9 +7,9 @@ import (
 func TestGetHostsAndPostJob(t *testing.T) {
 	const host1 = "a"
 	const host2 = "b"
-	var hostlist = HostList{"p_code", []string{host1,host2}}
+	var hostlist = HostList{"p_code", []string{host1, host2}}
 	var ps = PyramidScheme{}
-	
+
 	if job := ps.PostJob(&hostlist); job.Hosts[0].Name != host1 {
 		t.Errorf("GetHosts(%v) = %+v, want %v", hostlist, job, host1)
 	}
@@ -17,4 +17,3 @@ func TestGetHostsAndPostJob(t *testing.T) {
 		t.Errorf("GetHosts(%v) = %+v, want %v", hostlist, job, host2)
 	}
 }
-
